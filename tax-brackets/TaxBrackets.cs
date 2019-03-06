ccc422243e1b3467154f4fa8c265bf36aeb894e1
Brandon Berney <brandonberney@protonmail.com>
2019-03-06 18:37:55 -0500

    core: some SERIOUS code optimization

--- tax-brackets/TaxBrackets.cs
+++ tax-brackets/TaxBrackets.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * Grading ID: M5477
  * Program: 2
- * Due Date: Mar 5 2019
+ * Due Date: Mar 7 2019
  * Course Section: 01
  * Description: This program determines a users marginal tax rate and the amount due.
  */
@@ -41,12 +41,12 @@ namespace tax_brackets
                 TIER5_HEAD_MAX = 204100, // The highest dollar amount for someone filing as the head in tier 5.
                 TIER6_HEAD_MAX = 510300, // The highest dollar amount for someone filing as the head in tier 6.
 
-                TIER1_SEPARATE_MAX = 9700,     // The highest dollar amount for someone filing separately in tier 1.
-                TIER2_SEPARATE_MAX = 39475,    // The highest dollar amount for someone filing separately in tier 2.
-                TIER3_SEPARATE_MAX = 84200,    // The highest dollar amount for someone filing separately in tier 3.
-                TIER4_SEPARATE_MAX = 160725,   // The highest dollar amount for someone filing separately in tier 4.
-                TIER5_SEPARATE_MAX = 204100,   // The highest dollar amount for someone filing separately in tier 5.
-                TIER6_SEPARATE_MAX = 306175;   // The highest dollar amount for someone filing seperately in tier 6.
+                TIER1_SEPARATE_MAX = 9700,   // The highest dollar amount for someone filing separately in tier 1.
+                TIER2_SEPARATE_MAX = 39475,  // The highest dollar amount for someone filing separately in tier 2.
+                TIER3_SEPARATE_MAX = 84200,  // The highest dollar amount for someone filing separately in tier 3.
+                TIER4_SEPARATE_MAX = 160725, // The highest dollar amount for someone filing separately in tier 4.
+                TIER5_SEPARATE_MAX = 204100, // The highest dollar amount for someone filing separately in tier 5.
+                TIER6_SEPARATE_MAX = 306175; // The highest dollar amount for someone filing seperately in tier 6.
 
             const double TIER1_RATE = .1, // The 2019 tax rate for tier 1.
                 TIER2_RATE = .12,         // The 2019 tax rate for tier 2.
@@ -59,9 +59,14 @@ namespace tax_brackets
             double bracketRate = 0, // The highest bracket percentage rate
                 incomeTaxOwed = 0;  // The money owed to Uncle Sam
 
-            uint reportedIncome,        // The income reported by the user
-                topMarginalBracket = 0, // The highest bracket the user is in
-                tierIncome;             // The income available for the next tier
+            uint reportedIncome, // The income reported by the user
+                
+                declaredMargin1 = 0, // The highest income for the marginal bracket 1.
+                declaredMargin2 = 0, // The highest income for the marginal bracket 2.
+                declaredMargin3 = 0, // The highest income for the marginal bracket 3.
+                declaredMargin4 = 0, // The highest income for the marginal bracket 4.
+                declaredMargin5 = 0, // The highest income for the marginal bracket 5.
+                declaredMargin6 = 0; // The highest income for the marginal bracket 6.
 
             bool incomeValid; // (Hopefully) our user entered a valid income
             incomeValid = uint.TryParse(income.Text, out reportedIncome);
@@ -70,291 +75,69 @@ namespace tax_brackets
             {
                 if (single.Checked)
                 {
-                    tierIncome = reportedIncome;
-
-                    if (tierIncome > TIER6_SINGLE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER6_SINGLE_MAX;
-                        incomeTaxOwed += tierIncome * TIER7_RATE;
-                        tierIncome = TIER6_SINGLE_MAX;
-                    }
-                    if (tierIncome >= TIER5_SINGLE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER5_SINGLE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER6_RATE;
-                        tierIncome = TIER5_SINGLE_MAX;
-                    }
-                    if (tierIncome >= TIER4_SINGLE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER4_SINGLE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER5_RATE;
-                        tierIncome = TIER4_SINGLE_MAX;
-                    }
-                    if (tierIncome >= TIER3_SINGLE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER3_SINGLE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER4_RATE;
-                        tierIncome = TIER3_SINGLE_MAX;
-                    }
-                    if (tierIncome >= TIER2_SINGLE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER2_SINGLE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER3_RATE;
-                        tierIncome = TIER2_SINGLE_MAX;
-                    }
-                    if (tierIncome >= TIER1_SINGLE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER1_SINGLE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER2_RATE;
-                        tierIncome = TIER1_SINGLE_MAX;
-                    }
-
-                    topMarginalBracket++;
-
-                    incomeTaxOwed += tierIncome * TIER1_RATE;
-                    tierIncome = 0;
+                    declaredMargin1 = TIER1_SINGLE_MAX;
+                    declaredMargin2 = TIER2_SINGLE_MAX;
+                    declaredMargin3 = TIER3_SINGLE_MAX;
+                    declaredMargin4 = TIER4_SINGLE_MAX;
+                    declaredMargin5 = TIER5_SINGLE_MAX;
+                    declaredMargin6 = TIER6_SINGLE_MAX;
                 }
-                else if (marriedJoint.Checked)
-                {
-                    tierIncome = reportedIncome;
-
-                    if (tierIncome > TIER6_JOINT_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER6_JOINT_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER7_RATE;
-                        tierIncome = TIER6_JOINT_MAX;
-                    }
-                    if (tierIncome >= TIER5_JOINT_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER5_JOINT_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER6_RATE;
-                        tierIncome = TIER5_JOINT_MAX;
-                    }
-                    if (tierIncome >= TIER4_JOINT_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER4_JOINT_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER5_RATE;
-                        tierIncome = TIER4_JOINT_MAX;
-                    }
-                    if (tierIncome >= TIER3_JOINT_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER3_JOINT_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER4_RATE;
-                        tierIncome = TIER3_JOINT_MAX;
-                    }
-                    if (tierIncome >= TIER2_JOINT_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER2_JOINT_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER3_RATE;
-                        tierIncome = TIER2_JOINT_MAX;
-                    }
-                    if (tierIncome >= TIER1_JOINT_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER1_JOINT_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER2_RATE;
-                        tierIncome = TIER1_JOINT_MAX;
-                    }
-
-                    topMarginalBracket++;
-
-                    incomeTaxOwed += tierIncome * TIER1_RATE;
-                    tierIncome = 0;
+                else if (marriedJoint.Checked) {
+                    declaredMargin1 = TIER1_JOINT_MAX;
+                    declaredMargin2 = TIER2_JOINT_MAX;
+                    declaredMargin3 = TIER3_JOINT_MAX;
+                    declaredMargin4 = TIER4_JOINT_MAX;
+                    declaredMargin5 = TIER5_JOINT_MAX;
+                    declaredMargin6 = TIER6_JOINT_MAX;
                 }
-                else if (headOfHousehold.Checked)
-                {
-                    tierIncome = reportedIncome;
-
-                    if (tierIncome > TIER6_HEAD_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER6_HEAD_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER7_RATE;
-                        tierIncome = TIER6_HEAD_MAX;
-                    }
-                    if (tierIncome >= TIER5_HEAD_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER5_HEAD_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER6_RATE;
-                        tierIncome = TIER5_HEAD_MAX;
-                    }
-                    if (tierIncome >= TIER4_HEAD_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER4_HEAD_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER5_RATE;
-                        tierIncome = TIER4_HEAD_MAX;
-                    }
-                    if (tierIncome >= TIER3_HEAD_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER3_HEAD_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER4_RATE;
-                        tierIncome = TIER3_HEAD_MAX;
-                    }
-                    if (tierIncome >= TIER2_HEAD_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER2_HEAD_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER3_RATE;
-                        tierIncome = TIER2_HEAD_MAX;
-                    }
-                    if (tierIncome >= TIER1_HEAD_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER1_HEAD_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER2_RATE;
-                        tierIncome = TIER1_HEAD_MAX;
-                    }
-
-                    topMarginalBracket++;
-
-                    incomeTaxOwed += tierIncome * TIER1_RATE;
-                    tierIncome = 0;
+                else if (headOfHousehold.Checked) {
+                    declaredMargin1 = TIER1_HEAD_MAX;
+                    declaredMargin2 = TIER2_HEAD_MAX;
+                    declaredMargin3 = TIER3_HEAD_MAX;
+                    declaredMargin4 = TIER4_HEAD_MAX;
+                    declaredMargin5 = TIER5_HEAD_MAX;
+                    declaredMargin6 = TIER6_HEAD_MAX;
                 }
-                else if (marriedSeparate.Checked)
-                {
-                    tierIncome = reportedIncome;
-
-                    if (tierIncome > TIER6_SEPARATE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER6_SEPARATE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER7_RATE;
-                        tierIncome = TIER6_SEPARATE_MAX;
-                    }
-                    if (tierIncome >= TIER5_SEPARATE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER5_SEPARATE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER6_RATE;
-                        tierIncome = TIER5_SEPARATE_MAX;
-                    }
-                    if (tierIncome >= TIER4_SEPARATE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER4_SEPARATE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER5_RATE;
-                        tierIncome = TIER4_SEPARATE_MAX;
-                    }
-                    if (tierIncome >= TIER3_SEPARATE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER3_SEPARATE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER4_RATE;
-                        tierIncome = TIER3_SEPARATE_MAX;
-                    }
-                    if (tierIncome >= TIER2_SEPARATE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER2_SEPARATE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER3_RATE;
-                        tierIncome = TIER2_SEPARATE_MAX;
-                    }
-                    if (tierIncome >= TIER1_SEPARATE_MAX)
-                    {
-                        topMarginalBracket++;
-
-                        tierIncome -= TIER1_SEPARATE_MAX;
-
-                        incomeTaxOwed += tierIncome * TIER2_RATE;
-                        tierIncome = TIER1_SEPARATE_MAX;
-                    }
-
-                    topMarginalBracket++;
-
-                    incomeTaxOwed += tierIncome * TIER1_RATE;
-                    tierIncome = 0;
+                else if (marriedSeparate.Checked) {
+                    declaredMargin1 = TIER1_SEPARATE_MAX;
+                    declaredMargin2 = TIER2_SEPARATE_MAX;
+                    declaredMargin3 = TIER3_SEPARATE_MAX;
+                    declaredMargin4 = TIER4_SEPARATE_MAX;
+                    declaredMargin5 = TIER5_SEPARATE_MAX;
+                    declaredMargin6 = TIER6_SEPARATE_MAX;
                 }
 
-                if (topMarginalBracket == 7)
-                {
-                    bracketRate = TIER7_RATE;
+                if (reportedIncome <= declaredMargin1) {
+                    incomeTaxOwed += reportedIncome * TIER1_RATE;
+                    bracketRate = TIER1_RATE;
                 }
-                else if (topMarginalBracket == 6)
-                {
-                    bracketRate = TIER6_RATE;
-                }
-                else if (topMarginalBracket == 5)
-                {
-                    bracketRate = TIER5_RATE;
-                }
-                else if (topMarginalBracket == 4)
-                {
-                    bracketRate = TIER4_RATE;
-                }
-                else if (topMarginalBracket == 3)
-                {
-                    bracketRate = TIER3_RATE;
-                }
-                else if (topMarginalBracket == 2)
-                {
+                if (reportedIncome <= declaredMargin2) {
+                    incomeTaxOwed += (reportedIncome - declaredMargin1) * TIER2_RATE;
                     bracketRate = TIER2_RATE;
                 }
-                else if (topMarginalBracket == 1)
-                {
-                    bracketRate = TIER1_RATE;
+                if (reportedIncome <= declaredMargin3) {
+                    incomeTaxOwed += (reportedIncome - declaredMargin2) * TIER3_RATE;
+                    bracketRate = TIER3_RATE;
+                }
+                if (reportedIncome <= declaredMargin4) {
+                    incomeTaxOwed += (reportedIncome - declaredMargin3) * TIER4_RATE;
+                    bracketRate = TIER4_RATE;
+                }
+                if (reportedIncome <= declaredMargin5) {
+                    incomeTaxOwed += (reportedIncome - declaredMargin4) * TIER5_RATE;
+                    bracketRate = TIER5_RATE;
+                }
+                if (reportedIncome <= declaredMargin6) {
+                    incomeTaxOwed += (reportedIncome - declaredMargin5) * TIER6_RATE;
+                    bracketRate = TIER6_RATE;
+                }
+                if (reportedIncome > declaredMargin6) {
+                    incomeTaxOwed += (reportedIncome - declaredMargin6) * TIER7_RATE;
+                    bracketRate = TIER7_RATE;
                 }
 
                 taxOwed.Text = incomeTaxOwed.ToString("C");
-                taxRate.Text = bracketRate.ToString("P0");
+                taxRate.Text = bracketRate.ToString("P0");   
             }
             else
             {
